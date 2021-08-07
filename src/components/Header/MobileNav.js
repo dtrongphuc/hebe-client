185998369277ee404ec303fb1d3d4cbdbe10dd26
Dương Trọng Phúc <dtrongphucdv@gmail.com>
2021-08-07 14:40:45 +0700

    cart dot

--- src/components/Header/MobileNav.js
+++ src/components/Header/MobileNav.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Container } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import logo from '../../assets/img/logo.webp';
@@ -7,6 +8,7 @@ import MobileBars from './MobileBars';
 
 export default function MobileNav({ categories, brands }) {
 	const [isNavOpen, setIsNavOpen] = useState(false);
+	const { total } = useSelector((state) => state.cart);
 
 	const handleOpenNav = () => {
 		setIsNavOpen(!isNavOpen);
@@ -16,7 +18,10 @@ export default function MobileNav({ categories, brands }) {
 		<div className='main__header mobile-header'>
 			<Container fluid='lg'>
 				<nav className='mobile-header__navbar'>
-					<Link to='/cart' className='mobile-cart-icon'></Link>
+					<Link
+						to='/cart'
+						className={`mobile-cart-icon ${total > 0 ? 'dot' : ''}`}
+					></Link>
 					<Link to='/'>
 						<img
 							src={logo}
