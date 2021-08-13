3ff3299a077fefe036b384e58eaa7f292b127f7d
Dương Trọng Phúc <dtrongphucdv@gmail.com>
2021-08-13 16:42:21 +0700

    fix add to cart price

--- src/components/ProductDetail/Form/ButtonAddToCart.js
+++ src/components/ProductDetail/Form/ButtonAddToCart.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { priceString } from 'utils/util';
 
 export default function ButtonAddToCart({ price, isSoldOut, quantity }) {
 	return (
 		<div className='atc__wrapper'>
 			<button type='submit' className='btn__atc' disabled={isSoldOut}>
 				<span className='text'>{isSoldOut ? 'Sold Out' : 'Add to Cart'}</span>$
-				{price}.00{quantity > 1 ? ` (${quantity})` : ''}
+				{priceString(price)}
+				{quantity > 1 ? ` (${quantity})` : ''}
 			</button>
 		</div>
 	);
